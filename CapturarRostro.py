24c1d1ec6f20ef2250a9a7b5eddceae659a1308f
carlos windows <orozcoalexander1996@gmail.com>
2022-04-26 15:08:08 -0500

    esp32cam

--- CapturarRostro.py
+++ CapturarRostro.py
@@ -1,43 +1,35 @@
 import cv2
 import urllib.request
 import numpy as np
-import os
-import imutils
-
+ 
 personName = 'Carlos'
 dataPath = 'D:/VCS/gitclonaciones/esp32face/data' #Cambia a la ruta donde hayas almacenado Data
 personPath = dataPath + '/' + personName
 
-if not os.path.exists(personPath):
-	print('Carpeta creada: ',personPath)
-	os.makedirs(personPath)
 f_cas= cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-eye_cascade=cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
-url='http://192.168.1.141/cam-lo.jpg'
+url='http://192.168.1.141/cam-hi.jpg'
 ##'''cam.bmp / cam-lo.jpg /cam-hi.jpg / cam.mjpeg '''
+cv2.namedWindow("Live Transmission", cv2.WINDOW_AUTOSIZE)
 count = 0
 while True:
     img_resp=urllib.request.urlopen(url)
     imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
     img=cv2.imdecode(imgnp,-1)
-    img =  imutils.resize(img, width=640)
-    auxFrame = img.copy()
-
-    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2BGRA)
     face=f_cas.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5)
     for x,y,w,h in face:
-        cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),2)
-        rostro = auxFrame[y:y+h,x:x+w]
-        
-        
-        rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite(personPath + '/rostro_{}.jpg'.format(count),rostro)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        roi_gray = cv2.resize(roi_gray,(150,150),interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(personPath + '/rostro_{}.jpg'.format(count),roi_gray)
         count = count + 1
-
-    cv2.imshow("live transmission",img)
-    key=cv2.waitKey(1)
-    if key == 27 or count >= 300:
-    		break
+  
  
-
+ 
+    cv2.imshow("live transmission",img)
+    key=cv2.waitKey(5)
+    if key==ord('q') or count >= 300:
+        break
+ 
 cv2.destroyAllWindows()
--- ReconocimientoFacial.py
+++ ReconocimientoFacial.py
@@ -2,45 +2,40 @@ import cv2
 import urllib.request
 import numpy as np
 import os
-import imutils
-
 dataPath = 'D:/VCS/gitclonaciones/esp32face/data' #Cambia a la ruta donde hayas almacenado Data
 imagePaths = os.listdir(dataPath)
 print('imagePaths=',imagePaths)
-
+f_cas= cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+eye_cascade=cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
+url='http://192.168.1.141/cam-hi.jpg'
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-# Leyendo el modelo
 face_recognizer.read('modeloLBPHFace.xml')
-url='http://192.168.1.141/cam-lo.jpg'
-
-
-faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-
+##'''cam.bmp / cam-lo.jpg /cam-hi.jpg / cam.mjpeg '''
+cv2.namedWindow("Live Transmission", cv2.WINDOW_AUTOSIZE)
+ 
 while True:
     img_resp=urllib.request.urlopen(url)
     imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
-    frame=cv2.imdecode(imgnp,-1)
-    frame =  imutils.resize(frame, width=640)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img=cv2.imdecode(imgnp,-1)
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     auxFrame = gray.copy()
-    faces = faceClassif.detectMultiScale(gray,1.3,5)
-    for (x,y,w,h) in faces:
+    face=f_cas.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5)
+    for x,y,w,h in face:
         rostro = auxFrame[y:y+h,x:x+w]
         rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
         result = face_recognizer.predict(rostro)
-        cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-	
-		# LBPHFace
+        cv2.putText(img,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
         if result[1] < 70:
-            cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(img,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+            cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),2)
         else:
-            cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-    cv2.imshow('frame',frame)
-    k = cv2.waitKey(1)
-    if ord('q')==cv2.waitKey(1):
+            cv2.putText(img,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+            cv2.rectangle(img, (x,y),(x+w,y+h),(0,0,255),2)
+ 
+ 
+    cv2.imshow("live transmission",img)
+    key=cv2.waitKey(5)
+    if key==ord('q'):
         break
-
+ 
 cv2.destroyAllWindows()
